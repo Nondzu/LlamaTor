0f98e382845cda353f2c45038a770653b5c43f11
Nondzu <kamil.nonckiewicz@gmail.com>
2023-09-17 11:00:05 +0200

    udapte generate to support multifilters

--- generate.py
+++ generate.py
@@ -1,30 +1,30 @@
 import argparse
 import json
-from huggingface_hub import HfApi
+from huggingface_hub import HfApi, ModelFilter
 from tqdm import tqdm
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--user', type=str, required=True)
-    parser.add_argument('--filter', type=str, default='')
+    parser.add_argument('--user', type=str, default=None)
+    parser.add_argument('--filter', type=str, nargs='+', default=[])
     args = parser.parse_args()
 
+    if args.user is None and len(args.filter) == 0:
+        raise ValueError("At least one of --user or --filter must be provided")
+
     username = args.user
-    filter_str = args.filter.lower()
+    filter_strs = [f.lower() for f in args.filter]
 
     api = HfApi()
-    print("Fetching all models")
-    all_models = api.list_models()
+    print("Fetching models")
+    model_filter = ModelFilter(author=username) if username else None
+    all_models = api.list_models(filter=model_filter)
 
-    print("Filtering models for user:", username)
-    user_models = [model for model in all_models if model.modelId.startswith(username + "/")]
+    print("Filtering models")
+    # filtered_models = [model for model in all_models if any(filter_str in model.modelId.lower() for filter_str in filter_strs)]
+    filtered_models = [model for model in all_models if all(filter_str in model.modelId.lower() for filter_str in filter_strs)]
 
-    print("Number of models for user:", len(user_models))
-
-    print("Applying filter:", filter_str)
-    filtered_models = [model for model in user_models if filter_str in model.modelId.lower()]
-
-    print("Number of models after applying filter:", len(filtered_models))
+    print("Number of models after filtering:", len(filtered_models))
 
     repo_table = []
     print("Processing models:")
@@ -34,7 +34,6 @@ def main():
             "branches": []
         }
 
-        # Get all branches and files for each branch
         print("Fetching branches for model:", model.modelId)
         try:
             git_refs = api.list_repo_refs(model.modelId, repo_type="model")
@@ -61,7 +60,7 @@ def main():
         repo_table.append(repo_data)
 
     print("Writing data to models.json")
-    with open('modelsss.json', 'w') as f:
+    with open('models-Airoboros.json', 'w') as f:
         json.dump(repo_table, f, indent=4)
 
     print("Done!")
